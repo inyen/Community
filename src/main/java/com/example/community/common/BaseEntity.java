93fa7076b673f7b2e6316d7698368520bf573431
인예원 <inyewon0704@gmail.com>
2025-08-18 09:10:00 +0900

    공통 BaseEntity 추가 (생성/수정시각 등 공통 필드)

--- src/main/java/com/example/community/common/BaseEntity.java
+++ src/main/java/com/example/community/common/BaseEntity.java
@@ -1,4 +1,26 @@
 package com.example.community.common;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "CREATED_TIME", nullable = false, updatable = false)
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_TIME")
+    private LocalDateTime updatedTime;
+
+    public LocalDateTime getCreatedTime() { return createdTime; }
+    public LocalDateTime getUpdatedTime() { return updatedTime; }
 }
