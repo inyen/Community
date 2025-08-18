d1c53a0695bb67c4ff5d0233ddca95c54e38b211
인예원 <inyewon0704@gmail.com>
2025-08-18 10:20:00 +0900

    에러 응답 모델 ErrorResponse 추가

--- src/main/java/com/example/community/common/error/ErrorResponse.java
+++ src/main/java/com/example/community/common/error/ErrorResponse.java
@@ -1,4 +1,24 @@
-package com.example.community.common.Error;
+package com.example.community.common.error;
 
-public class ErrorResponse {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErrorResponse (
+        String code,
+        String message,
+        int status,
+        LocalDateTime timestamp,
+        List<FieldError> fieldErrors
+) {
+    public static ErrorResponse of(HttpStatus status, String code, String message, List<FieldError> fieldErrors) {
+        return new ErrorResponse(code, message, status.value(), LocalDateTime.now(), fieldErrors);
+    }
+
+    public record FieldError(String field, String reason) {
+        public static FieldError of(String field, String reason) {return new FieldError(field, reason);}
+    }
 }
