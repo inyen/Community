d15dacb161255e5692769b49793e671cfc11a4ce
인예원 <inyewon0704@gmail.com>
2025-08-18 09:50:00 +0900

    Board 엔티티 및 Repository 생성

--- src/main/java/com/example/community/board/BoardRepository.java
+++ src/main/java/com/example/community/board/BoardRepository.java
@@ -1,4 +1,10 @@
 package com.example.community.board;
 
-public class BoardRepository {
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    @EntityGraph(attributePaths = "user")
+    Page<Board> findByDeleteYn(String deleteYn, Pageable pageable);
 }
