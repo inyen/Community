ed20a4b9a4d030d650e4a2cbde9693d06fcee2c7
인예원 <inyewon0704@gmail.com>
2025-08-19 09:10:00 +0900

    BoardService 인터페이스: getDetail(), update(), delete() 정의

--- src/main/java/com/example/community/board/BoardService.java
+++ src/main/java/com/example/community/board/BoardService.java
@@ -1,4 +1,9 @@
 package com.example.community.board;
 
-public class BoardService {
+import com.example.community.board.dto.BoardDtos;
+
+public interface BoardService {
+    BoardDtos.DetailRes getDetail(Long id);
+    void update(Long id, Long currentUserId, BoardDtos.UpdateReq req);
+    void delete(Long id, Long currentUserId);
 }
