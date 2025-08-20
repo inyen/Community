d833b0a9fa370d759f52f65f57f91b9bffcf1a08
인예원 <inyewon0704@gmail.com>
2025-08-20 09:35:00 +0900

    WebConfig 추가: LoginUserArgumentResolver 등록

--- src/main/java/com/example/community/common/WebConfig.java
+++ src/main/java/com/example/community/common/WebConfig.java
@@ -1,4 +1,19 @@
 package com.example.community.common;
 
-public class WebConfig {
+import com.example.community.common.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(loginUserArgumentResolver);
+    }
 }
