d39781d85a8a5dbb387093fed526377392dd273c
인예원 <inyewon0704@gmail.com>
2025-08-26 10:15:00 +0900

    docs(user): LoginDtos에 DTO 용도/필드 의미 주석 추가

--- src/main/java/com/example/community/user/dto/LoginDtos.java
+++ src/main/java/com/example/community/user/dto/LoginDtos.java
@@ -1,7 +1,9 @@
 package com.example.community.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
-
+/*
+로그인 응답 DTO
+ */
 public class LoginDtos {
     //엔티티의 로그인 아디이명(userId)
     public record LoginRequest(@NotBlank String userId, @NotBlank String password) {}
