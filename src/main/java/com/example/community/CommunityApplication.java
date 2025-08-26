00b4895a308245b1757c277b83b5600035412238
인예원 <inyewon0704@gmail.com>
2025-08-26 09:20:00 +0900

    docs(common): JPA Auditing 및 BaseEntity createdTime/updatedTime 자동 기록 주석

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -3,8 +3,10 @@ package com.example.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
+/*
+애플리케이션 엔트리, 전역 설정
+ */
+@EnableJpaAuditing	//BaseEntity의 createdTime/updatedTime 자동 기록 활성화
 @SpringBootApplication
 public class CommunityApplication {
 	public static void main(String[] args) {
